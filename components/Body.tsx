ed11e499ff0b6a2fd0dc3ae9bdb0c04dd1e62cc4
NwaguChima <nwaguchima48@gmail.com>
2022-07-25 20:31:02 +0100

    setup body

---
+++ components/Body.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface BodyProps {}
+
+const Body: React.FC<BodyProps> = ({}) => {
+  return (
+    <div>
+      <h1>Body</h1>
+    </div>
+  );
+};
+
+export default Body;
