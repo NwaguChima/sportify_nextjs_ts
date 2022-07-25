d2612c3ca1cbe3405cd808a88942192fea611657
NwaguChima <nwaguchima48@gmail.com>
2022-07-25 20:30:03 +0100

    setup sidebar component

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <main>
+      <Sidebar />
+      <Body />
+      <Right />
+    </main>
+  );
 };
 
 export default Dashboard;
