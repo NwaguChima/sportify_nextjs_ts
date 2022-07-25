70d8fde210db24b954ca1375332f40db31c59046
NwaguChima <nwaguchima48@gmail.com>
2022-07-25 20:28:21 +0100

    setup dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Dashboard from "../components/Dashboard";
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Spotify - Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Dashboard />
     </div>
   );
 };
