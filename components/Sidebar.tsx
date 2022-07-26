29aa781db9674d20af8c3478935e8f2891a01028
NwaguChima <nwaguchima48@gmail.com>
2022-07-26 02:07:30 +0100

    rendered sportify logo

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 interface SidebarProps {}
 
 const Sidebar: React.FC<SidebarProps> = () => {
-  return <div>Sidebar</div>;
+  return (
+    <section>
+      <Image
+        src="https://rb.gy/xkacau"
+        width={56}
+        height={56}
+        objectFit="contain"
+      />
+    </section>
+  );
 };
 
 export default Sidebar;
