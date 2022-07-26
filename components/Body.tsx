599904a7d68a948b7b6661115c0ed92965b75d43
NwaguChima <nwaguchima48@gmail.com>
2022-07-26 02:53:39 +0100

    added plugins

--- components/Body.tsx
+++ components/Body.tsx
@@ -5,7 +5,7 @@ interface BodyProps {}
 
 const Body: React.FC<BodyProps> = ({}) => {
   return (
-    <section className="">
+    <section className="bg-black ml24 py-4 space-y-8 md:max-w-6xl  flex-grow md:mr-2.5">
       <Search />
     </section>
   );
