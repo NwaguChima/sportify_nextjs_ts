86a5437d60a7e7cf10180476e2737584c8ba1583
NwaguChima <nwaguchima48@gmail.com>
2022-07-28 19:24:07 +0100

    rendered heart and styled div

--- components/Track.tsx
+++ components/Track.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { ImHeadphones } from "react-icons/im";
+import { AiFillHeart } from "react-icons/ai";
 
 interface TrackProps {
   track: any;
@@ -6,12 +8,14 @@ interface TrackProps {
 }
 
 export const Track: React.FC<TrackProps> = ({ track, chooseTrack }) => {
+  const [hasLiked, setHasLiked] = React.useState<boolean>(false);
+
   return (
     <div className="flex items-center justify-between space-x-20 cursor-default hover:bg-white/10 py-2 px-4 rounded-lg group transition ease-out">
       <div className="flex items-center">
         <img
           src={track.albumUrl}
-          alt=""
+          alt="track cover"
           className="rounded-xl h-12 w-12 object-cover mr-3"
         />
         <div>
@@ -21,6 +25,21 @@ export const Track: React.FC<TrackProps> = ({ track, chooseTrack }) => {
           <p className="text-[rgb(179,179,179)] text-[13px] font-semibold group-hover:text-white">
             {track.artist}
           </p>
+        </div>
+
+        <div className="md:ml-auto flex items-center space-x-2.5">
+          <div className="text-white flex space-x-1 text-sm font-semibold">
+            <ImHeadphones className="text-lg" />
+            <h4>{track.popularity}</h4>
+          </div>
+          <div className="flex items-center rounded-full border-2 border-[#262626] w-[85px] h-10 relative cursor-pointer group-hover:border-white/40">
+            <AiFillHeart
+              className={`text-xl ml-3 icon ${
+                hasLiked ? "text-[#1ed760]" : "text-[#868686]"
+              }`}
+              onClick={() => setHasLiked(!hasLiked)}
+            />
+          </div>
         </div>
       </div>
     </div>
