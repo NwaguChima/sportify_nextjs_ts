5254043839a054c680f9fbbcc2f42086990ba28b
NwaguChima <nwaguchima48@gmail.com>
2022-07-28 20:02:30 +0100

    setup page and icon

--- components/Right.tsx
+++ components/Right.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { HiOutlineShieldCheck } from "react-icons/hi";
+import { MdOutlineSettings } from "react-icons/md";
+import { BiBell } from "react-icons/bi";
+import { ViewGridIcon } from "@heroicons/react/solid";
 
-interface RightProps {}
+interface RightProps {
+  spotifyApi: any;
+  chooseTrack: any;
+}
 
 const Right: React.FC<RightProps> = ({}) => {
-  return <div>Right</div>;
+  return (
+    <section className="p-4 space-y-8 pr-8">
+      <div className="flex space-x-2 items-center justify-between">
+        {/* Icons */}
+        <div className="flex items-center space-x-4 border-2 border-[#262626] rounded-full h-12 py-3 px-4"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Right;
