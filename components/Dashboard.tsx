cf07ff266e7a2caa4f65dc446fb5bc2764fa9e4c
NwaguChima <nwaguchima48@gmail.com>
2022-07-28 20:47:57 +0100

    rendered player

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import SpotifyWebApi from "spotify-web-api-node";
 import { playingTrackState } from "../atom/playerAtom";
 import Body from "./Body";
+import { Player } from "./Player";
 import Right from "./Right";
 import Sidebar from "./Sidebar";
 
@@ -13,16 +15,28 @@ const spotifyApi = new SpotifyWebApi({
 interface DashboardProps {}
 
 export const Dashboard: React.FC<DashboardProps> = ({}) => {
+  const { data: session } = useSession();
+  const accessToken = session?.accessToken;
   const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  useEffect(() => {
+    setShowPlayer(true);
+  }, []);
 
   const chooseTrack = (track: any) => {
     setPlayingTrack(track);
   };
+
   return (
     <main className="flex max-h-screen min-w-max bg-black lg:pb-24">
       <Sidebar />
       <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
       <Right spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
+
+      <div>
+        <Player />
+      </div>
     </main>
   );
 };
---
+++ components/Player.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface PlayerProps {}
+
+export const Player: React.FC<PlayerProps> = ({}) => {
+  return <div>hello</div>;
+};
