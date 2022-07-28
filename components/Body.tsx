27ca49e62ecde8e74b38e063e6d06fc1f1f2758a
NwaguChima <nwaguchima48@gmail.com>
2022-07-28 14:37:28 +0100

    searching for tracks

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,11 +1,49 @@
-import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import SpotifyWebApi from "spotify-web-api-node";
 import { Search } from "./Search";
 
-interface BodyProps {}
+interface BodyProps {
+  spotifyApi: SpotifyWebApi;
+}
 
-const Body: React.FC<BodyProps> = ({}) => {
+const Body: React.FC<BodyProps> = ({ spotifyApi }) => {
+  const { data: session } = useSession();
+  const accessToken = session?.accessToken;
+
   const [search, setSearch] = useState<string>("");
-  const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [searchResults, setSearchResults] = useState<any>([]);
+  const [newReleases, setNewReleases] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (!accessToken) return;
+
+    // @ts-ignore
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  // searching for tracks
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+
+    spotifyApi.searchTracks(search).then((res) => {
+      setSearchResults(
+        res.body.tracks?.items.map((track) => {
+          return {
+            id: track.id,
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: track.album.images[0].url,
+            popularity: track.popularity,
+          };
+        })
+      );
+    });
+  }, [search, accessToken]);
+
+  console.log("searchResults", searchResults);
 
   return (
     <section className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl  flex-grow md:mr-2.5">
--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-node";
 import Body from "./Body";
 import Right from "./Right";
 import Sidebar from "./Sidebar";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+});
 
 interface DashboardProps {}
 
@@ -9,7 +14,7 @@ export const Dashboard: React.FC<DashboardProps> = ({}) => {
   return (
     <main>
       <Sidebar />
-      <Body />
+      <Body spotifyApi={spotifyApi} />
       <Right />
     </main>
   );
