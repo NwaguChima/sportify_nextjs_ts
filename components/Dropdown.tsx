bd3ff815516efb882566b3ce1cea74eb7d004c55
NwaguChima <nwaguchima48@gmail.com>
2022-07-28 20:10:51 +0100

    created dropdown

---
+++ components/Dropdown.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface DropdownProps {}
+
+const Dropdown: React.FC<DropdownProps> = ({}) => {
+  return <div>Hello</div>;
+};
+
+export default Dropdown;
--- components/Right.tsx
+++ components/Right.tsx
@@ -3,6 +3,7 @@ import { HiOutlineShieldCheck } from "react-icons/hi";
 import { MdOutlineSettings } from "react-icons/md";
 import { BiBell } from "react-icons/bi";
 import { ViewGridIcon } from "@heroicons/react/solid";
+import Dropdown from "./Dropdown";
 
 interface RightProps {
   spotifyApi: any;
@@ -14,7 +15,13 @@ const Right: React.FC<RightProps> = ({}) => {
     <section className="p-4 space-y-8 pr-8">
       <div className="flex space-x-2 items-center justify-between">
         {/* Icons */}
-        <div className="flex items-center space-x-4 border-2 border-[#262626] rounded-full h-12 py-3 px-4"></div>
+        <div className="flex items-center space-x-4 border-2 border-[#262626] rounded-full h-12 py-3 px-4">
+          <HiOutlineShieldCheck className="text-[#ccc] text-xl" />
+          <MdOutlineSettings className="text-[#ccc] text-xl" />
+          <BiBell className="text-[#ccc] text-xl" />
+        </div>
+        {/* Profile */}
+        <Dropdown />
       </div>
     </section>
   );
